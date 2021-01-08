dd61da23c1095c5a73c48bbc648488bf28e123ed
Ryan Pope <poperyan73@gmail.com>
2021-01-08 14:51:53 -0600

    Add new code to read from CAN network

--- can_read_write.py
+++ can_read_write.py
@@ -3,36 +3,27 @@
 
 from flask import Flask
 import can
+import os
 import sys
 import queue
 
 # info handled specified by https://docs.google.com/document/d/1kUU54jQZAB9nwCM-iA96Kj0fXJ6RNw9nAcBffzji5cU/edit
-# full list of Parameter IDs at https://en.wikipedia.org/wiki/OBD-II_PIDs
-PARAMETER_IDS = {'VEHICLE_BATTERY_PERCENT': 0x2F,
-                 'VEHICLE_SPEED': 0x0D,
-                 }
 
+stats = {
+    "soc": 0
+}
 
-REQUEST_ID = 0x7DF
-REPLY_ID = 0x7E8
-
-
-class FilteredBufferReader(can.BufferedReader):
-    def __init__(self, filter_parameter):
-        """
-        :param filter_parameter: from PARAMETER_IDS
-        """
-        can.BufferedReader.__init__(self)
-        self.buffer = queue.Queue(0)
-        self.filter_parameter = filter_parameter
+class DataListener(can.Listener):
+    def __init__(self):
+        pass
 
     def on_message_received(self, msg):
-        if msg.arbitration_id == REPLY_ID and msg.data[2] == self.filter_parameter:
-            self.buffer.put(msg)
-
+        if msg.arbitration_id == 0x6B0:
+            stats["soc"] = msg.data[6] / 2
 
 # establish interface w CAN BUS
 try:
+    # os.system("sudo /sbin/ip link set can0 up type can bitrate 250000")
     bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 
 except OSError:
@@ -40,16 +31,14 @@ except OSError:
     sys.exit()
 
 # BUS readers filter and queue messages for reading
-speed_reader = FilteredBufferReader([PARAMETER_IDS['VEHICLE_SPEED']])
-battery_percent_reader = FilteredBufferReader(PARAMETER_IDS['VEHICLE_BATTERY_PERCENT'])
+reader = DataListener()
 
 # notifier distributes messages to readers
 notifier = can.Notifier(bus,
-                        listeners=[speed_reader, battery_percent_reader],
+                        listeners=[reader],
                         timeout=5.0)
 
 app = Flask(__name__)
-
 
 @app.route('/get-speed')
 def get_speed():
@@ -69,17 +58,7 @@ def get_speed():
 
 @app.route('/get-battery-percent')
 def get_battery_percent():
-    bus.send(can.Message(arbitration_id=REQUEST_ID,
-                         data=[0x02, 0x01, PARAMETER_IDS['VEHICLE_BATTERY_PERCENT'], 0x00, 0x00, 0x00, 0x00, 0x00]))
-
-    rec_msg = battery_percent_reader.get_message()
-
-    # for debug, TODO: remove for production
-    if rec_msg:
-        return str(rec_msg.data[3])
-    else:
-        return str(-1)
-
+    return str(stats["soc"])
 
 if __name__ == '__main__':
     app.run()
