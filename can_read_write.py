44e7f096666c83a0cef17d3d2ed0a15cb6e484ab
Ryan Pope <poperyan73@gmail.com>
2021-03-11 19:07:41 -0600

    Update HUD design and server to allow for more data

--- can_read_write.py
+++ can_read_write.py
@@ -15,11 +15,45 @@ import serial
 
 stats = {
     "soc": 0,
-    "temp": 0
+    "temp": 0,
+    "pvolt": 0,
+    "svolt": 0,
+    "speed": 0,
+    "error": []
 }
 
+error_messages = [
+    # DTC #1 Status
+    "Discharge Limit Enforcement Fault",
+    "Charger Safety Relay Fault",
+    "Internal Hardware Fault",
+    "Internal Heatsink Thermistor Fault",
+    "Internal Software Fault",
+    "High Cell Voltage Too High Fault",
+    "Low Cell Voltage Too Low Fault",
+    "Pack Too Hot Fault",
+    # Reserved
+    # DTC #2 Status
+    "Internal Communication Fault",
+    "Cell Balancing Stuck Off Fault",
+    "Weak Cell Fault",
+    "Low Cell Voltage Fault",
+    "Open Wiring Fault",
+    "Current Sensor Fault",
+    "Highest Cell Voltage Over 5 Volts Fault",
+    "Cell ASIC Fault",
+    "Weak Pack Fault",
+    "Fan Monitor Fault",
+    "Thermistor Fault",
+    "External Communication Fault",
+    "Redundant Power Supply Fault",
+    "High Voltage Isolation Fault",
+    "Input Power Supply Fault",
+    "Charge Limit Enforcement Fault"
+]
+
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=3) 
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) 
 handler = logging.FileHandler('/home/pi/log/hud.log')
 handler.setLevel(logging.ERROR)
 app.logger.addHandler(handler)
@@ -27,10 +61,18 @@ app.logger.addHandler(handler)
 @app.route('/get-data')
 def get_data():
     line = ser.readline().decode("ascii")
+    ser.flush()
     if line:
         line = line.strip()
-        if len(line.split()) == 2:
-            stats["soc"], stats["temp"] = line.split()
+        if len(line.split()) == 6:
+            line_parts = line.split()
+            stats["soc"] = line_parts[0]
+            stats["temp"] = line_parts[1]
+            stats["pvolt"] = line_parts[2]
+            stats["svolt"] = line_parts[3]
+            stats["speed"] = line_parts[4]
+            error_str = line_parts[5]
+            stats["error"] = [error_messages[i] for i, val in error_str.reverse() if val == "1"]
         else:
             app.logger.error(line)
     return json.dumps(stats)
