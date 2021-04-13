faefa8896ae9a1c634ac600442a5931be1095ba4
ryan-hackku <ryan@hackku.org>
2021-04-12 19:32:49 -0500

    Add animations for gauges

--- static/gauge.js
+++ static/gauge.js
@@ -13,7 +13,9 @@ class Gauge {
         this.high = config.high;
         this.title = config.title;
         this.units = config.units;
-		this.noDraw = config.noDraw;
+
+        let averageR = (this.r1 + this.r2) / 2;
+        this.circumference = 2 * Math.PI * averageR;
 
         this.value = this.low;
         this.svgContainer = document.getElementById(config.svgId);
@@ -26,18 +28,23 @@ class Gauge {
         let start = [this.r2 * Math.cos(this.startAngle) + this.cx, this.r2 * Math.sin(this.startAngle) + this.cy];
         let end = [this.r2 * Math.cos(this.endAngle) + this.cx, this.r2 * Math.sin(this.endAngle) + this.cy];
 
-		if (!this.noDraw) {
-			this.gaugeFill = document.createElementNS(svgns, "path");
-			this.gaugeFill.setAttribute('stroke', 'transparent');
-			this.gaugeFill.setAttribute('fill', '#0f0');
-			this.gaugeFill.setAttribute('class', 'fillGauge');
-			this.svgContainer.appendChild(this.gaugeFill);
+        this.gaugeFill = document.createElementNS(svgns, "circle");
+        this.gaugeFill.setAttribute('cx', this.cx);
+        this.gaugeFill.setAttribute('cy', this.cy);
+        this.gaugeFill.setAttribute('r', (this.r1 + this.r2) / 2);
+        this.gaugeFill.setAttribute('stroke', '#0f0');
+        this.gaugeFill.setAttribute('stroke-width', `${(this.r2 - this.r1)}px`);
+        this.gaugeFill.setAttribute('stroke-dashoffset', `${this.circumference / 4}px`);
+        this.gaugeFill.setAttribute('stroke-dasharray', `${this.circumference}px`);
+        this.gaugeFill.setAttribute('transform-origin', `${this.cx}px ${this.cy}px`);
+        this.gaugeFill.setAttribute('fill', 'transparent');
+        this.gaugeFill.setAttribute('class', 'gaugePath');
+        this.svgContainer.appendChild(this.gaugeFill);
 
-			this.gaugeBorder = document.createElementNS(svgns, "path");
-			this.gaugeBorder.setAttribute('stroke', 'black');
-			this.gaugeBorder.setAttribute('fill', 'transparent');
-			this.svgContainer.appendChild(this.gaugeBorder);
-		}
+        this.gaugeBorder = document.createElementNS(svgns, "path");
+        this.gaugeBorder.setAttribute('stroke', 'black');
+        this.gaugeBorder.setAttribute('fill', 'transparent');
+        this.svgContainer.appendChild(this.gaugeBorder);
 
         this.valueText = document.createElementNS(svgns, "text");
         this.valueText.setAttribute('x', this.cx);
@@ -90,6 +97,17 @@ class Gauge {
         return percent * (this.endAngle - this.startAngle + 2 * Math.PI) + this.startAngle;
     }
 
+    computePercent(value) {
+        let percent = (value - this.low) / (this.high - this.low);
+        if (percent < 0) {
+            percent = 0;
+        }
+        if (percent > 1) {
+            percent = 1;
+        }
+        return percent;
+    }
+
     drawBaseGauge() {
         let start1 = [this.r1 * Math.cos(this.startAngle) + this.cx, this.r1 * Math.sin(this.startAngle) + this.cy];
         let start2 = [this.r2 * Math.cos(this.startAngle) + this.cx, this.r2 * Math.sin(this.startAngle) + this.cy];
@@ -103,49 +121,47 @@ class Gauge {
         this.gaugeBorder.setAttribute('d', svgString);
     }
 
-    drawFilledGauge() {
-        let angle = this.computeAngle(this.value);
+    getPath(value) {
+        let angle = this.computeAngle(value);
         let start1 = [this.r1 * Math.cos(this.startAngle) + this.cx, this.r1 * Math.sin(this.startAngle) + this.cy];
         let start2 = [this.r2 * Math.cos(this.startAngle) + this.cx, this.r2 * Math.sin(this.startAngle) + this.cy];
         let end1 = [this.r1 * Math.cos(angle) + this.cx, this.r1 * Math.sin(angle) + this.cy];
         let end2 = [this.r2 * Math.cos(angle) + this.cx, this.r2 * Math.sin(angle) + this.cy];
+        let startMiddle = [(start1[0] + start2[0]) / 2, (start1[1] + start2[1]) / 2];
+        let endMiddle = [(end1[0] + end2[0]) / 2, (end1[1] + end2[1]) / 2];
+        let r = (this.r1 + this.r2) / 2;
         let sweep = 0;
         if (angle > this.startAngle + Math.PI) {
             sweep = 1;
         }
 
-        let svgString = `M ${start1[0]} ${start1[1]} A ${this.r1} ${this.r1} 0 ${sweep} 1 ${end1[0]} ${end1[1]}
-                         L ${end2[0]} ${end2[1]} A ${this.r2} ${this.r2} 0 ${sweep} 0 ${start2[0]} ${start2[1]}
-                         L ${start2[0]} ${start2[1]}`;
+        let svgString = `M ${startMiddle[0]} ${startMiddle[1]} A ${r} ${r} 0 ${sweep} 1 ${endMiddle[0]} ${endMiddle[1]}`;
 
-        this.gaugeFill.setAttribute('d', svgString);
+        return svgString;
     }
 
-	drawMinMax() {
-        this.minText.textContent = this.low;
-        this.maxText.textContent = this.high;
-	}
+    drawFilledGauge() {
+        let percent = this.computePercent(this.value);
+        let offset = (1 - percent) * (this.circumference * 3 / 4) + (this.circumference / 4);
+        this.gaugeFill.setAttribute('stroke-dashoffset', offset + 'px');
+    }
 
     drawText() {
         this.labelSpan.textContent = this.title;
         this.valueSpan.textContent = this.value + this.units;
+        this.minText.textContent = this.low;
+        this.maxText.textContent = this.high;
     }
 
     draw() {
-		if (!this.noDraw) {
-			this.drawFilledGauge();
-			this.drawBaseGauge();
-			this.drawMinMax();
-		}
+        this.drawFilledGauge();
+        this.drawBaseGauge();
         this.drawText();
     }
 
     setValue(newVal) {
         this.value = newVal;
-		if (!this.noDraw) {
-			this.drawFilledGauge();
-			this.drawMinMax();
-		}
+        this.drawFilledGauge();
         this.drawText();
     }
 }
