5f1c6f55c9b251dc529707b64ba2d1712a5d6569
Drake Prebyl <drakeprebyl@gmail.com>
2022-07-04 11:45:06 -0500

    Fix hostapd fixer

--- config-files/hostapd-fixer.sh
+++ config-files/hostapd-fixer.sh
@@ -3,7 +3,7 @@
 # Super janky script to make hostapd work
 # Called from `sudo crontab` multiple times after boot
 
-if (sudo service hostapd status | tail -n 5 | grep --quiet "Could not connect to kernel driver"); then
+if (sudo service hostapd status | tail -n 7 | grep --quiet "Could not connect to kernel driver"); then
   sudo service hostapd restart
   echo "fail" >> /home/pi/hostapd-fixer.txt
 else
