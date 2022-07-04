d47f8d010ee75491356a1687af281b18fd2e50f5
Drake Prebyl <drakeprebyl@gmail.com>
2022-07-04 11:45:34 -0500

    Transmit telemetry to cloud if internet available

--- config-files/start-hud.sh
+++ config-files/start-hud.sh
@@ -4,5 +4,5 @@
 # (both browsers open on the same display)
 
 export DISPLAY=:0
-chromium-browser http://localhost:5000/static/index.html --kiosk --new-window --user-data-dir=/tmp/browser1 &
-chromium-browser http://localhost:5000/static/dashcam.html --kiosk --new-window --window-position="800,0" --user-data-dir=/tmp/browser2 &
+chromium-browser http://localhost:5000/static/index.html --kiosk --new-window --user-data-dir=/tmp/browser1 --disable-web-security &
+chromium-browser http://localhost:5000/static/dashcam.html --kiosk --new-window --window-position="800,0" --user-data-dir=/tmp/browser2 --use-fake-ui-for-media-stream --disable-web-security &
